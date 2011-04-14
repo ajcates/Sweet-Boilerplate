7a762df785c51958ddb190a7ee981c9e5f31c654
ajcates <aj@ajcates.com>
2011-04-13 17:06:33 -0700

    ack!

--- app/settings/databases.php
+++ app/settings/databases.php
@@ -1,23 +1,24 @@
 <?php
-//location of the mysql db
 if(stristr($_SERVER['HTTP_HOST'], 'local') === false) {
+    //Live DB
 	SweetFramework::getClass('lib', 'Config')->setAll('databases', array(
 		'default' => array(
 			'driver' => 'My_SQL',
 			'host' => 'localhost',
 			'username' => 'ajcates',
-			'password' => 'aldo20',
-			'databaseName' => 'holster-data'
+			'password' => '',
+			'databaseName' => ''
 		)
 	));
 } else {	
+    //Dev DB
 	SweetFramework::getClass('lib', 'Config')->setAll('databases', array(
 		'default' => array(
 			'driver' => 'My_SQL',
 			'host' => 'localhost',
 			'username' => 'root',
-			'password' => 'dworkram',
-			'databaseName' => 'holster-data'
+			'password' => '',
+			'databaseName' => ''
 		)
 	));
 }

33253f9b3cf4b8596e445d0d7e0905312099a43c
ajcates <aj@ajcates.com>
2011-04-13 17:11:07 -0700

    updated the databaes file

--- app/settings/databases.php
+++ app/settings/databases.php
@@ -1,22 +1,22 @@
 <?php
-if(stristr($_SERVER['HTTP_HOST'], 'local') === false) {
-    //Live DB
-	SweetFramework::getClass('lib', 'Config')->setAll('databases', array(
-		'default' => array(
-			'driver' => 'My_SQL',
-			'host' => 'localhost',
-			'username' => 'ajcates',
-			'password' => '',
-			'databaseName' => ''
-		)
-	));
-} else {	
+if(stristr(@$_SERVER['USER'], 'ajcates') || stristr(@$_SERVER['HTTP_HOST'], 'ajcates') || stristr(@$_SERVER['HTTP_HOST'], 'localhost')) {
     //Dev DB
 	SweetFramework::getClass('lib', 'Config')->setAll('databases', array(
 		'default' => array(
 			'driver' => 'My_SQL',
 			'host' => 'localhost',
 			'username' => 'root',
+			'password' => '',
+			'databaseName' => ''
+		)
+	));
+} else {	
+    //Live DB
+	SweetFramework::getClass('lib', 'Config')->setAll('databases', array(
+		'default' => array(
+			'driver' => 'My_SQL',
+			'host' => 'localhost',
+			'username' => 'ajcates',
 			'password' => '',
 			'databaseName' => ''
 		)
